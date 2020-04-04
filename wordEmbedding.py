218539356000b5b2a454e7c348e0b2c439aabd07
Daniel Hathcock <danielchathcock98@gmail.com>
2020-04-04 01:28:33 -0600

    word embeddings

--- wordEmbedding.py
+++ wordEmbedding.py
@@ -1,3 +1,12 @@
 import fasttext
 from pathlib import Path
 
+MODEL_PATH = Path('englishVecs/cc.en.300.bin')
+
+embeddingModel = None
+
+def prepare_sequence(sentence):
+    if embeddingModel is None:
+        embeddingModel = fasttext.load_model(MODEL_PATH)
+
+    return [embeddingModel[word] for word in sentence]
