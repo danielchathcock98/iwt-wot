0f5c616207fd3c310e7ea087f8a5a8ce0743d0f2
sherrysarkar <sherry.sarkar@outlook.com>
2020-04-04 01:03:56 -0700

    First write up of Model 2

--- model.py
+++ model.py
@@ -1,10 +1,17 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+EMBEDDING_DIM = 300
+HIDDEN_DIM = 4
+LEARNING_RATE = 0.2
+LSTM_LAYERS = 1
+DROPOUT = 0
+EPOCHS = 30
+
 class Model2(nn.Module):
 
     def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
-        super(BasicPOSTagger, self).__init__()
+        super(Model2, self).__init__()
         #############################################################################
         # TODO: Define and initialize anything needed for the forward pass.
         # You are required to create a model with:
@@ -12,31 +19,29 @@ class Model2(nn.Module):
         # an LSTM layer: that takes word embeddings as input and outputs hidden states
         # a Linear layer: maps from hidden state space to tag space
         #############################################################################
-        self.embedding_dim = embedding_dim
+        #self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
 
-        self.encoder = nn.Embedding(vocab_size, embedding_dim)
+        #self.encoder = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=LSTM_LAYERS, dropout=DROPOUT)
-        self.decoder = nn.Linear(hidden_dim, tagset_size)
+        self.decoder = nn.Linear(hidden_dim, 1)  # We output a single score now.
 
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
 
-    def forward(self, sentence):
+    def forward(self, encoded_sentence):
         tag_scores = None
         #############################################################################
         # TODO: Implement the forward pass.
         # Given a tokenized index-mapped sentence as the argument,
         # compute the corresponding scores for tags
-        # returns:: tag_scores (Tensor)
+        # returns:: score (Tensor)
         #############################################################################
 
-        encoded = self.encoder(sentence)
-        lstmOut, _ = self.lstm(encoded.view(-1, 1, self.embedding_dim))
-        tag_scores = self.decoder(lstmOut.view(-1, self.hidden_dim))
+        #encoded = self.encoder(sentence)
+        #lstmOut, _ = self.lstm(encoded.view(-1, 1, self.embedding_dim))
+        lstmOut, _ = self.lstm(encoded_sentence)
+        score = self.decoder(lstmOut.view(-1, self.hidden_dim))
 
-        #############################################################################
-        #                             END OF YOUR CODE                              #
-        #############################################################################
-        return tag_scores
+        return score
