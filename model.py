183861d5f8bbdbf269c7fcd81cd86d973c47c906
Daniel Hathcock <danielchathcock98@gmail.com>
2020-04-23 15:13:19 -0600

    fixes model3

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ class Model3(nn.Module):
 
     def __init__(self, hidden_dim, lstm_layers, dropout):
         super(Model3, self).__init__()
-        self.hidden_dim = HIDDEN_DIM
+        self.hidden_dim = hidden_dim
 
         self.lstm1 = nn.LSTM(EMBEDDING_DIM, hidden_dim, num_layers=lstm_layers, dropout=dropout)
         self.lstm2 = nn.LSTM(EMBEDDING_DIM, hidden_dim, num_layers=lstm_layers, dropout=dropout)
@@ -38,10 +38,10 @@ class Model3(nn.Module):
         self.decoder = nn.Linear(2*hidden_dim, 2)  
         self.softmax_layer = nn.Softmax(dim=0)
 
-    def forward(self, encoded_sentence):
+    def forward(self, encoded_sentence1, encoded_sentence2):
 
-        _, (lstm_hn1, _) = self.lstm1(encoded_sentence.view(-1, 1, EMBEDDING_DIM))
-        _, (lstm_hn2, _) = self.lstm2(encoded_sentence.view(-1, 1, EMBEDDING_DIM))
+        _, (lstm_hn1, _) = self.lstm1(encoded_sentence1.view(-1, 1, EMBEDDING_DIM))
+        _, (lstm_hn2, _) = self.lstm2(encoded_sentence2.view(-1, 1, EMBEDDING_DIM))
 
         # Concatenating might be done wrong.
         decoded = self.decoder(torch.cat((lstm_hn1.view(self.hidden_dim), lstm_hn2.view(self.hidden_dim)), 0)) 
