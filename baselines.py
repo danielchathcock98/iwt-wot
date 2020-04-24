0f277217f3afb46fb02ad7b7d83fd0712ca21584
sherrysarkar <sherry.sarkar@outlook.com>
2020-04-23 21:00:12 -0700

    baseline code for task 2 finished

--- baselines.py
+++ baselines.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 from scipy import stats
 
-from preprocessing import TASK_1, EXTRA_TRAIN_TASK_1, readData, readDataTask2
+from preprocessing import TASK_1, TASK_2, EXTRA_TRAIN_TASK_1, EXTRA_TRAIN_TASK_2, readData, readDataTask2
 
 
 def mode_baseline(files):
@@ -22,7 +22,7 @@ def mean_baseline(files):
         all_scores.append(score)
     return sum(all_scores) / len(all_scores)
 
-def task2_baseline(files)
+def task2_baseline(files):
     raw_data = readDataTask2(files)
     all_labels = []
     for data_point in raw_data:
@@ -45,9 +45,27 @@ def evaluate_baseline(baseline_score, files):
     rmse = math.sqrt(square_error / val_examples)
     return rmse
 
-if __name__ == '__main__':
-    print('mode baseline:')
-    print(evaluate_baseline(mode_baseline([TASK_1 / 'train.csv', EXTRA_TRAIN_TASK_1, TASK_1 / 'dev.csv']), [TASK_1 / 'test.csv']))
+def evaluate_baseline_task2(baseline_score, files):
+    print(baseline_score)
+    raw_data = readDataTask2(files)
 
-    print('mean baseline:')
-    print(evaluate_baseline(mean_baseline([TASK_1 / 'train.csv', EXTRA_TRAIN_TASK_1, TASK_1 / 'dev.csv']), [TASK_1 / 'test.csv']))
+    num_correct = 0
+    val_examples = 0
+    for data_point in raw_data:
+        (sent1, replStart1, replEnd1), repl1, (sent2, replStart2, replEnd2), repl2, label = data_point
+        if label == baseline_score:
+            num_correct += 1
+        val_examples += 1
+
+    return num_correct/val_examples
+
+if __name__ == '__main__':
+    #print('mode baseline:')
+    #print(evaluate_baseline(mode_baseline([TASK_1 / 'train.csv', EXTRA_TRAIN_TASK_1, TASK_1 / 'dev.csv']), [TASK_1 / 'test.csv']))
+
+    #print('mean baseline:')
+    #print(evaluate_baseline(mean_baseline([TASK_1 / 'train.csv', EXTRA_TRAIN_TASK_1, TASK_1 / 'dev.csv']), [TASK_1 / 'test.csv']))
+
+    print('task2 baseline')
+    print(evaluate_baseline_task2(task2_baseline([TASK_2 / 'train.csv', EXTRA_TRAIN_TASK_2, TASK_2 / 'dev.csv']), [TASK_2 / 'test.csv']))
+
